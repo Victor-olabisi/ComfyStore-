2aab3b3bbf6e479ff6e16c6186ecb344f7279fac
victor <oguntayovictor65@gmail.com>
2023-12-03 14:57:18 -0800

    fix minor bug in header ui

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -4,11 +4,11 @@ const Header = () => {
   return (
     <header className="  text-neutral-content  bg-neutral p-2">
       <div className="align-element flex justify-center md:justify-end ">
-        <div className=" flex gap-x-4 items-center">
-          <Link to={"/login"} className=" text-white text-xs capitalize sm:text-sm ">
+        <div className=" flex gap-x-6  items-center">
+          <Link to={"/login"} className="link link-hover text-xs capitalize sm:text-sm ">
             sign in / guest
           </Link>
-          <Link to={"/register"} className=" text-white text-xs capitalize sm:text-sm ">
+          <Link to={"/register"} className="link link-hover text-xs capitalize sm:text-sm ">
             create account
           </Link>
         </div>
