cc7ee979326b15dbef421eac3b1ac987ab7cc4eb
victor <oguntayovictor65@gmail.com>
2023-12-10 15:28:51 -0800

    bug

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -16,7 +16,7 @@ export const loader = async ({ request }) => {
 
 const Products = () => {
   const { products, meta } = useLoaderData();
-  const { params } = useLoaderData()
+  // const { params } = useLoaderData()
   return (
     <>
       <Filter />
